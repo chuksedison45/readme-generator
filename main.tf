df877d182883d790e37b59d4043e8081e09a375a
Edison Chukwuemeka <chuksedison45@yahoo.com>
2026-03-16 19:14:47 -0700

    Initial Commit: Readme Generator Agents Project

---
+++ main.tf
@@ -0,0 +1,104 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1" # You can change this to your preferred region
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+module "s3_bucket" {
+  source      = "./modules/s3"
+  bucket_name = "ed45-readme-generator-output-bucket-${random_string.suffix.result}"
+}
+
+
+# Role specifically for the Lambda function to run
+module "lambda_execution_role" {
+  source             = "./modules/iam"
+  role_name          = "ed45-ReadmeGeneratorLambdaExecutionRole"
+  service_principals = ["lambda.amazonaws.com"]
+  policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  ]
+}
+
+# Role specifically for the Bedrock Agent to use
+module "bedrock_agent_role" {
+  source             = "./modules/iam"
+  role_name          = "ed45-ReadmeGeneratorBedrockAgentRole"
+  service_principals = ["bedrock.amazonaws.com"]
+  policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonBedrockFullAccess"
+  ]
+}
+
+output "readme_bucket_name" {
+  description = "The name of the S3 bucket where README files are stored."
+  value       = module.s3_bucket.bucket_id
+}
+
+
+data "archive_file" "repo_scanner_zip" {
+  type        = "zip"
+  source_dir  = "${path.root}/src/repo_scanner"
+  output_path = "${path.root}/dist/repo_scanner.zip"
+}
+
+resource "aws_lambda_function" "repo_scanner_lambda" {
+  function_name    = "ed45-RepoScannerTool"
+  role             = module.lambda_execution_role.role_arn # Uses the dedicated Lambda role
+  filename         = data.archive_file.repo_scanner_zip.output_path
+  handler          = "lambda_function.handler"
+  runtime          = "python3.12"
+  timeout          = 30 # Increased timeout for cloning
+  source_code_hash = data.archive_file.repo_scanner_zip.output_base64sha256
+
+  # This line adds the 'git' command to our Lambda environment
+  layers = ["arn:aws:lambda:us-east-1:553035198032:layer:git-lambda2:8"]
+}
+
+module "repo_scanner_agent" {
+  source                  = "./modules/bedrock_agent"
+  agent_name              = "ed45-Repo_Scanner_Agent"
+  agent_resource_role_arn = module.bedrock_agent_role.role_arn # Uses the dedicated Bedrock role
+  instruction             = "Your job is to use the scan_repo tool to get a file list from a public GitHub URL. You are a helpful AI assistant. When a user provides a GitHub URL, you must use the available tool to scan it."
+}
+
+resource "aws_bedrockagent_agent_action_group" "repo_scanner_action_group" {
+  agent_id           = module.repo_scanner_agent.agent_id
+  agent_version      = "DRAFT"
+  action_group_name  = "ScanRepoAction"
+  action_group_state = "ENABLED"
+
+  action_group_executor {
+    lambda = aws_lambda_function.repo_scanner_lambda.arn
+  }
+
+  api_schema {
+    payload = file("${path.root}/repo_scanner_schema.json")
+  }
+}
+
+# This resource grants the Bedrock Agent permission to invoke our Lambda function
+resource "aws_lambda_permission" "allow_bedrock_to_invoke_lambda" {
+  statement_id  = "AllowBedrockToInvokeRepoScannerLambda"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.repo_scanner_lambda.function_name
+  principal     = "bedrock.amazonaws.com"
+  source_arn    = module.repo_scanner_agent.agent_arn
+}
